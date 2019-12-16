31cca673af039ebf42cc6a87c95607dd6b344a81
spektr001 <sanek777.28.03.2000@gmail.com>
2019-12-16 21:07:40 +0200

    Convertor

---
+++ currency-converter.js
@@ -0,0 +1,36 @@
+const axios = require(`axios`);
+
+const getExchangeRate = async (fromCurrency, toCurrency) => {
+  try {
+    const response = await axios.get('http://data.fixer.io/api/latest?access_key=c2d1d626a186df8a3304a794d4bc9582&format=1');   
+    const rate = response.data.rates;
+    const euro = 1 / rate[fromCurrency];
+    const exchangeRate = euro * rate[toCurrency];    
+    return exchangeRate;
+  }catch (error) {
+    throw new Error(`Unable to get currency ${fromCurrency} and  ${toCurrency}`);
+  }
+};
+
+const getCountries = async (currencyCode) => {
+  try {
+    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+    return response.data.map(country => country.name);
+  } catch (error) {
+    throw new Error(`Unable to get countries that use ${currencyCode}`);
+  }
+};
+
+const convert = async (fromCurrency, toCurrency, amount) => {
+  const exchangeRate = await getExchangeRate(fromCurrency, toCurrency);
+  const countries = await getCountries(toCurrency);
+  const convertedAmount = (amount * exchangeRate).toFixed(2);
+  return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following countries: ${countries}`;
+}
+
+convert('USD', 'HRK', 20)
+  .then((message) => {
+    console.log(message);
+  }).catch((error) => {
+    console.log(error.message);
+  });
